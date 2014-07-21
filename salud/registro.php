94d4f37d8a35cd2f75d5712111f6c1e3cd696582
macastro <macastro@espol.edu.ec>
2014-07-20 21:29:46 -0500

    añadidos input type date and email a registro
    añadidos input type date and email a registro

--- salud/registro.php
+++ salud/registro.php
@@ -25,58 +25,58 @@
 		
 		
 		
-		<div class="wrap-content zerogrid">
+		<div id="contenedor_registro">
 			<div class="row block05" align="center">
-				<div class="title"><h2>Registro de Usuario</h2></div>
+				<div id="titulo"><h2>Registro de Usuario</h2></div>
 				<div class="col-2-3">
-					<div class="wrap-col">
+					<div class="contenedor_registro1">
 						<article>
-							<div class="reg">
-								
-					
-								<form action="registrado.php" method="post" name="contact">
+                            <form action="registrado.php" method="post" name="contact">
 
-					  
-									<div class="contactForm nuevo_paciente">
-										<h3>Datos Personales</h3>
 
-										<div class="leftCol">
-										<label for="usuario">Nombre de Usuario</label>
-											<input tabindex="1" type="text" name="usuario" id="usuario" class="txtBox" value="" />
-											<br class="clear" />
-											<br> 										   
-											<label for="contrasena">Contrasena</label>
-											<input tabindex="2" type="text" name="contrasena" id="contrasena" class="txtBox" value="" />
-											<br class="clear" />
-											<br>
-											<label for="nombres">Nombres</label>
-											<input tabindex="1" type="text" name="nombres" id="nombres" class="txtBox" value="" />
-											<br class="clear" />
-											<br> 										   
-											<label for="apellidos">Apellidos</label>
-											<input tabindex="2" type="text" name="apellidos" id="apellidos" class="txtBox" value="" />
-											<br class="clear" />
-											<br>
-											<label for="nacimiento" style="padding-right: 0; width: 140px" >Fecha de nacimiento</label>
-											<input tabindex="8" type="text" name="nacimiento" id="nacimiento" class="txtBox" value="" />
-											<br class="clear" />
-											<br>
-											<label for="genero">Genero</label>
-											<input tabindex="4" type="text" name="genero" id="genero" class="txtBox" value="" />
-											<br class="clear" />
-											<br>
-											<label for="email">E-mail</label>
-											<input tabindex="6" type="text" name="email" id="email" class="txtBox" value="" />
-											<br class="clear" />
-											<br>
-											
-										</div> 
-									</div>
-									<p align="center"><input type="submit" class="button" value="Enviar" name="submit"></p>
-								</form>
-								
-								
-							</div>
+                                <div class="nuevo_registro">
+                                    <h3>Datos Personales</h3>
+
+                                    <div class="leftCol">
+                                        <label for="usuario">Nombre de Usuario</label>
+                                        <input tabindex="1" type="text" name="usuario" id="usuario" class="txtBox" value="" />
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="contraseña">Contrase&ntilde;a</label>
+                                        <input tabindex="2" type="text" name="contrasena" id="contrasena" class="txtBox" value="" />
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="contraseña">Repetir Contrase&ntilde;a</label>
+                                        <input tabindex="3" type="text" name="contrasena" id="contrasena" class="txtBox" value="" />
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="nombres">Nombres</label>
+                                        <input tabindex="4" type="text" name="nombres" id="nombres" class="txtBox" value="" />
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="apellidos">Apellidos</label>
+                                        <input tabindex="5" type="text" name="apellidos" id="apellidos" class="txtBox" value="" />
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="nacimiento" style="padding-right: 0; width: 140px" >Fecha de nacimiento</label>
+                                        <input tabindex="6" type="date" name="nacimiento" min="1913-01-01" max="2014-07-07"/>
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="genero">Genero</label><br>
+                                        <input tabindex="7" type="radio" name="genero" value="masculino">Masculino<br>
+                                        <input tabindex="7" type="radio" name="genero" value="femenino">Femenino
+
+                                        <br class="clear" />
+                                        <br>
+                                        <label for="email">E-mail</label>
+                                        <input tabindex="8" type="email" name="email" id="email" class="txtBox" value="" />
+                                        <br class="clear" />
+                                        <br>
+
+                                    </div>
+                                </div>
+                                <p align="center"><input type="submit" class="button" value="Enviar" name="submit"></p>
+                            </form>
 						</article>
 					</div>
 				</div>
