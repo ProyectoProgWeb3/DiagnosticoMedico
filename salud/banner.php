6f42fbd1c4c7fc2478fe0c035792c4e36f71a034
Karen <kmtorres@espol.edu.ec>
2014-07-25 22:20:38 -0500

    Update banner.php

--- salud/banner.php
+++ salud/banner.php
@@ -1,36 +1,35 @@
 <?php
 
 echo <<< HERE
-<nav id="galeria" class="grid_19">
-		
-		<div class="slider-wrapper">
-			<div id="slider">
-				<div class="slide1">
-					<img src="img/img1.jpg" alt="" />
-				</div>
-				<div class="slide2">
-					<img src="img/img2.jpg" alt="" />
-				</div>
-				<div class="slide3">
-					<img src="img/img3.jpg" alt="" />
-				</div>
-				<div class="slide4">
-					<img src="img/img4.jpg" alt="" />
-				</div> 
-			</div>
-			<div id="slider-direction-nav"></div>
-			<div id="slider-control-nav"></div>
-		</div>
-		
-		<script type="text/javascript">
-		$(document).ready(function() {
-			var slider = $('#slider').leanSlider({
-				directionNav: '#slider-direction-nav',
-				controlNav: '#slider-control-nav'
-			});
-		});
-		</script>
+
+<div id="slider1">
+  <div class="slider-wrapper theme-default">
+    <div id="slider" class="nivoSlider"> <img src="M_S/Images/toystory.jpg" data-thumb="M_S/Images/toystory.jpg" alt="" /> <img src="M_S/Images/up.jpg" data-thumb="M_S/Images/up.jpg" alt="" title="Disfrute de  un agradable momento..." /></a> <img src="M_S/Images/walle.jpg" data-thumb="M_S/Images/walle.jpg" alt="" data-transition="slideInLeft" /> <img src="M_S/Images/nemo.jpg" data-thumb="M_S/Images/nemo.jpg" alt="" title="#htmlcaption" /> </div>
+    <div id="htmlcaption" class="nivo-html-caption"> <strong>Somos los mejores</strong> a nivel<em>NACIONAL</em>  </div>
+  </div>
+</div>
+
+<!--aqui llama mi carpeta M_S -->
+
+
+
+
+
+<!-- xxxxxxxxxxxxxxxxxxxx para el slider       xxxxxxxxxxxxxxxxxxxxxxxxxxx -->
+<script type="text/javascript" src="M_S/scripts/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="M_S/scripts/jquery.nivo.slider.js"></script>
+<script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+    
+	<link rel="stylesheet" href="M_S/themes/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="M_S/themes/light/light.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="M_S/themes/dark/dark.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="M_S/themes/bar/bar.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="M_S/CSS_Menu/nivo-slider.css" type="text/css" media="screen" />   
+ <!-- xxxxxxxxxxxxxxxxxxxx            slider       xxxxxxxxxxxxxxxxxxxxxxxxxxx -->   
 	
-</nav>
-HERE;
+
 ?>
