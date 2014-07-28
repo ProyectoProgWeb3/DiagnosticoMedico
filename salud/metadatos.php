64c8d3b697f228ab75ed0f61f736b97fcf7c75b8
macastro <macastro@espol.edu.ec>
2014-07-27 23:00:26 -0500

    introduccion a bootstrap
    introduccion a bootstrap

--- salud/metadatos.php
+++ salud/metadatos.php
@@ -4,10 +4,16 @@ echo <<< HERE
 <head>
 	<title>Salud 24/7</title>
 	<link rel="stylesheet" type="text/css" href="css/960_24_col.css" />
-	<link rel="stylesheet" type="text/css" href="css/estilos.css" />	
+	<link rel="stylesheet" type="text/css" href="css/estilos.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
 	<script src="js/modernizr-2.6.1.min.js"></script>
   <script src="js/jquery.js"></script>
   <script src="slideshow/lean-slider.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootstrap.js"></script>
   <link rel="stylesheet" href="slideshow/lean-slider.css" type="text/css" />   
 	<link rel="stylesheet" href="slideshow/sample-styles.css" type="text/css" />
   <link rel="shortcut icon" href="img/estetoscopio.ico" type="image/x-icon" />   		
