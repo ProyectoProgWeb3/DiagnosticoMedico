59761495371ee933d5729870760447d38b947447
carfeper <carfeper@espol.edu.ec>
2014-08-12 10:52:39 -0500

    loginboton

--- salud/header.php
+++ salud/header.php
@@ -3,7 +3,8 @@
 echo <<< HERE
 <div id="cabecera" class="grid_24">
 	<nav id="menuTop" class="grid_24">
-		<p> <a href="https://www.facebook.com/"><img id="sociales" src="img/facebookicon.png"/></a><a href="https://plus.google.com/up/search/"><img id="sociales" src="img/googleicon.png"/></a><a href="https://twitter.com/"><img id="sociales" src="img/twittericon.png"/></a></p>
+		<p> <a href="https://www.facebook.com/"><img id="sociales" src="img/facebookicon.png"/></a><a href="https://plus.google.com/up/search/"><img id="sociales" src="img/googleicon.png"/></a><a href="https://twitter.com/"><img id="sociales" src="img/twittericon.png"/></a>
+		<br><a href="iniciar.php"><img src="img/login.png"/></a><a href="salir.php"><img src="img/logout.png"/></a></p>
   </nav>
 </div>	
 HERE;
