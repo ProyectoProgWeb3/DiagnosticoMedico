7ea252d4453e133543ba9f9d1b6ccd00b275e35f
carfeper <carfeper@espol.edu.ec>
2014-08-12 11:48:17 -0500

    botones

--- salud/header.php
+++ salud/header.php
@@ -4,7 +4,7 @@ echo <<< HERE
 <div id="cabecera" class="grid_24">
 	<nav id="menuTop" class="grid_24">
 		<p> <a href="https://www.facebook.com/"><img id="sociales" src="img/facebookicon.png"/></a><a href="https://plus.google.com/up/search/"><img id="sociales" src="img/googleicon.png"/></a><a href="https://twitter.com/"><img id="sociales" src="img/twittericon.png"/></a>
-		<br><a href="iniciar.php"><img src="img/login.png"/></a><a href="salir.php"><img src="img/logout.png"/></a></p>
+		<br><br><a href="registro.php"><img src="img/registro.jpg"/></a><a href="iniciar.php"><img src="img/entrar.png"/></a></p>
   </nav>
 </div>	
 HERE;
