0c8fdfe01ec67bb38db6eeda10d9924ed220a658
macastro <macastro@espol.edu.ec>
2014-08-25 23:07:30 -0500

    Pruebas desde Servidor

--- salud/configuracion/bd.php
+++ salud/configuracion/bd.php
@@ -4,6 +4,6 @@
  * Configuracion para conexion de base de datos
  */
 define("DB_HOST", "127.0.0.1");
-define("DB_NAME", "proyecto");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_NAME", "u307700064_proye");
+define("DB_USER", "u307700064_root");
+define("DB_PASS", "root12");
