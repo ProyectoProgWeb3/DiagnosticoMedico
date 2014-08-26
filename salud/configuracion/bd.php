8051c59def10ea55a0319bc245f3649aa8eb1d94
macastro <macastro@espol.edu.ec>
2014-08-25 23:45:57 -0500

    Pruebas en servidor

--- salud/configuracion/bd.php
+++ salud/configuracion/bd.php
@@ -3,7 +3,7 @@
 /**
  * Configuracion para conexion de base de datos
  */
-define("DB_HOST", "31.220.16.41");
+define("DB_HOST", "mysql.hostinger.es");
 define("DB_NAME", "u307700064_proye");
 define("DB_USER", "u307700064_root");
 define("DB_PASS", "root12");
